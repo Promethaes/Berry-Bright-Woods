95f49dd8d4bf419a3e8cc493137c3e39c0696a65
Angie <50060168+Promethaes@users.noreply.github.com>
2022-09-20 15:09:24 -0400

    Import Fairy GUI and DevConsole
    - modified fairyGUI to use new input system thanks to https://github.com/fairygui/FairyGUI-unity/pull/184
    - fixed DevConsole not instantiating by commenting out static class with 0 references. hopefully that doesnt break anything lol

---
+++ BerryBrightWoods/Assets/FairyGUI/Scripts/Core/StageEngine.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace FairyGUI
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class StageEngine : MonoBehaviour
+    {
+        public int ObjectsOnStage;
+        public int GraphicsOnStage;
+
+        public static bool beingQuit;
+
+        void Start()
+        {
+            useGUILayout = false;
+        }
+
+        void LateUpdate()
+        {
+            Stage.inst.InternalUpdate();
+
+            ObjectsOnStage = Stats.ObjectCount;
+            GraphicsOnStage = Stats.GraphicsCount;
+        }
+
+        void OnGUI()
+        {
+            Stage.inst.HandleGUIEvents(Event.current);
+        }
+
+        void OnApplicationQuit()
+        {
+            if (Application.isEditor)
+            {
+                beingQuit = true;
+                UIPackage.RemoveAllPackages();
+            }
+        }
+    }
+}
